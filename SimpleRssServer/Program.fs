bf7c5d91b83579344549916d2fc8168463e2919d
Roald Ruiter <roaldruiter@gmail.com>
2024-08-07 16:09:50 +0200

    formatting works

--- SimpleRssServer/Program.fs
+++ SimpleRssServer/Program.fs
@@ -16,16 +16,22 @@ let fetchRssFeed url =
 let handleRequest (context: HttpListenerContext) =
     async {
         let! items = fetchRssFeed myFeed
-        let itemHtml = 
-            items 
+
+        let itemHtml =
+            items
             |> Seq.map (fun (title, link) -> sprintf "<li><a href='%s'>%s</a></li>" link.Href title.Value)
             |> String.concat ""
+
         let responseString = sprintf "<h1>RSS Feed</h1><ul>%s</ul>" itemHtml
-        
+
         let buffer = Encoding.UTF8.GetBytes(responseString)
         context.Response.ContentLength64 <- int64 buffer.Length
         context.Response.ContentType <- "text/html"
-        do! context.Response.OutputStream.WriteAsync(buffer, 0, buffer.Length) |> Async.AwaitTask
+
+        do!
+            context.Response.OutputStream.WriteAsync(buffer, 0, buffer.Length)
+            |> Async.AwaitTask
+
         context.Response.OutputStream.Close()
     }
 
@@ -34,18 +40,17 @@ let startServer (prefixes: string list) =
     prefixes |> List.iter listener.Prefixes.Add
     listener.Start()
     printfn "Listening..."
-    
+
     let rec loop () =
         async {
             let! context = listener.GetContextAsync() |> Async.AwaitTask
             do! handleRequest context
             return! loop ()
         }
-    
+
     loop ()
 
 [<EntryPoint>]
 let main argv =
-    startServer ["http://localhost:5000/"]
-    |> Async.RunSynchronously
+    startServer [ "http://localhost:5000/" ] |> Async.RunSynchronously
     0
