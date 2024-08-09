8c19146b3ca0c42ab07f8e2c898b8d2c4755b78b
Roald Ruiter <roaldruiter@gmail.com>
2024-08-09 15:38:38 +0200

    move request methods to request

--- SimpleRssServer/Program.fs
+++ SimpleRssServer/Program.fs
@@ -1,50 +1,7 @@
 ﻿open System.Net
 open System.IO
-open System.Text
-open FSharp.Data
+
 open SimpleRssServer.Request
-
-type Rss = XmlProvider<"https://roaldin.ch/feed.xml">
-
-let fetchRssFeed (url: string) =
-    async {
-        let! rss = Rss.AsyncLoad(url)
-        return rss.Entries |> Seq.map (fun item -> item.Title, item.Link)
-    }
-
-let fetchAllRssFeeds (urls: string list) =
-    urls
-    |> List.map fetchRssFeed
-    |> Async.Parallel
-    |> Async.RunSynchronously
-    |> Seq.concat
-
-let handleRequest (context: HttpListenerContext) =
-    async {
-        let rssFeeds = getRssUrls context.Request.Url.Query
-
-        let items =
-            match rssFeeds with
-            | Some urls -> fetchAllRssFeeds urls
-            | None -> Seq.empty
-
-        let itemHtml =
-            items
-            |> Seq.map (fun (title, link) -> sprintf "<li><a href='%s'>%s</a></li>" link.Href title.Value)
-            |> String.concat ""
-
-        let responseString = sprintf "<h1>RSS Feed</h1><ul>%s</ul>" itemHtml
-
-        let buffer = Encoding.UTF8.GetBytes(responseString)
-        context.Response.ContentLength64 <- int64 buffer.Length
-        context.Response.ContentType <- "text/html"
-
-        do!
-            context.Response.OutputStream.WriteAsync(buffer, 0, buffer.Length)
-            |> Async.AwaitTask
-
-        context.Response.OutputStream.Close()
-    }
 
 let startServer (prefixes: string list) =
     let listener = new HttpListener()
