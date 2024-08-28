7500fd6e44ae0d613520b4a1d31edb44787a99e9
Serveo VPS <root@serveovps.nl>
2024-08-28 17:54:29 +0200

    add fail2ban

--- server-conf/sync-server-settings.sh
+++ server-conf/sync-server-settings.sh
@@ -3,12 +3,13 @@
 # Script to copy server settings to the current directory
 
 # Source files
+FAIL2BAN_CONF="/etc/fail2ban/jail.local"
 NGINX_CONF="/etc/nginx/nginx.conf"
 SYSTEMD_SERVICE="/etc/systemd/system/mfrssreader-server.service"
 
 # Target directory (current directory)
 TARGET_DIR=$(pwd)
 
-
+cp -i "$FAIL2BAN_CONF" "$TARGET_DIR/jail.local"
 cp -i "$NGINX_CONF" "$TARGET_DIR/nginx.conf"
 cp -i "$SYSTEMD_SERVICE" "$TARGET_DIR/mfrssreader-server.service"
