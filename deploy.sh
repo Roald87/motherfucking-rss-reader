bf0bc551a44f3daa024de8988d7b333b9c885ce5
Roald Ruiter <roaldruiter@gmail.com>
2024-08-30 14:53:43 +0200

    keep rss-cache between deployments

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 git pull
 dotnet publish
 sudo systemctl stop mfrssreader-server.service
-rm -rf /var/www/rss/*
+find /var/www/rss/* -not -name 'rss-cache' -not -path '/var/www/rss/rss-cache/*' -exec rm -rf {} +
 cp -r SimpleRssServer/bin/Release/net8.0/publish/* /var/www/rss/
 sudo systemctl start mfrssreader-server.service
