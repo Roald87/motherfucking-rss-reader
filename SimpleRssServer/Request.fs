1f7843acebac31517d8aa5f452e5c39e34de7b5d
Roald Ruiter <roaldruiter@gmail.com>
2024-09-13 06:57:22 +0200

    add header when fetching

--- SimpleRssServer/Request.fs
+++ SimpleRssServer/Request.fs
@@ -35,6 +35,10 @@ let getAsync (client: HttpClient) (url: string) (lastModified: DateTimeOffset op
         try
             let request = new HttpRequestMessage(HttpMethod.Get, url)
 
+            request.Headers.UserAgent.ParseAdd(
+                "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
+            )
+
             match lastModified with
             | Some date -> request.Headers.IfModifiedSince <- date
             | None -> ()
