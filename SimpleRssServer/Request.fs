8fdcf2b165eb1dc626d0625b0371e0241bd5ba79
Roald Ruiter <roaldruiter@gmail.com>
2024-09-14 09:24:49 +0200

    set user agent to mfrssreader and add version number

--- SimpleRssServer/Request.fs
+++ SimpleRssServer/Request.fs
@@ -5,6 +5,7 @@ open System
 open System.IO
 open System.Net
 open System.Net.Http
+open System.Reflection
 open System.Text
 open System.Web
 
@@ -34,10 +35,8 @@ let getAsync (client: HttpClient) (url: string) (lastModified: DateTimeOffset op
     async {
         try
             let request = new HttpRequestMessage(HttpMethod.Get, url)
-
-            request.Headers.UserAgent.ParseAdd(
-                "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
-            )
+            let version = Assembly.GetExecutingAssembly().GetName().Version.ToString()
+            request.Headers.UserAgent.ParseAdd($"motherfucking-rss-reader/{version}")
 
             match lastModified with
             | Some date -> request.Headers.IfModifiedSince <- date
